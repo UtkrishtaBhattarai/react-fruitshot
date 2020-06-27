c5c667447a5bad401933e4c8ba014ca9d1947bd9
Utkrishta Bhattarai <bhattarai.utk1@gmail.com>
2020-06-27 18:27:30 +0545

    Login and Registration Complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import LoginComponent from "./components/login/LoginComponent";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container } from "reactstrap";
+import AdminLogin from "./components/admin/HeaderComponent";
+import mainComponent from "./components/Main";
+import NoMatch from "./components/NoMatch";
+import HeaderComponent from "./components/header/HeaderComponent";
+
+import RegisterComponent from "./components/register/RegisterComponent";
+// import DashboardComponent from "./components/dashboard/DashboardComponent";
+// import DetailComponent from "./components/DetailComponent";
+// import PrivateRoute from "./components/PrivateRoute";
+// import ProfileComponent from "./components/profile/ProfileComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <HeaderComponent></HeaderComponent>
+        {/* <AdminLogin></AdminLogin> */}
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
-
 export default App;
