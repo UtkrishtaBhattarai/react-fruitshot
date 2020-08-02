1e0cbd3422ffa47cf7fd43535f817b8c803adf00
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-08-02 23:26:49 +0545

    newtestprodadd

---
+++ src/components/admin/addnewproducts/producttestadd.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import axios from "axios";
+import SimpleReactValidator from "simple-react-validator";
+import AdminHeaderComponent from "../adminheader/AdminHeaderComponent";
+import { Link, Redirect } from 'react-router-dom';
+import './productcomponent.css'
