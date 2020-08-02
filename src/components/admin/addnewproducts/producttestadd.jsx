7a3d48dce88710ff27b066d7098bb9b5fefc5693
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-08-02 23:27:26 +0545

    secondtestprodadd

--- src/components/admin/addnewproducts/producttestadd.jsx
+++ src/components/admin/addnewproducts/producttestadd.jsx
@@ -4,3 +4,19 @@ import SimpleReactValidator from "simple-react-validator";
 import AdminHeaderComponent from "../adminheader/AdminHeaderComponent";
 import { Link, Redirect } from 'react-router-dom';
 import './productcomponent.css'
+class ProductComponent extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        name: "",
+        description: "",
+        usage: "",
+        currentFile: null,
+        image: "",
+        price: "",
+        productid: null,
+        products: []
+      };
+      this.validator = new SimpleReactValidator();
+    }
